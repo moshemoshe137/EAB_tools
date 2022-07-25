9715c63cfc0f5965faedb0936cc6529052f7d6ba
Moshe Rubin <mosherubin137@gmail.com>
2022-07-25 17:57:42 -0500

    Add `TestLoadDf::test_specify_file_type_csv`

--- EAB_tools/tests/io/test_io.py
+++ EAB_tools/tests/io/test_io.py
@@ -11,13 +11,14 @@ from EAB_tools import load_df
 @pytest.fixture(autouse=True)
 def _test_cache_cleanup() -> Iterator[None]:
     # Before the test
+    cache_path = Path(__file__).parent / "data" / ".eab_tools_cache"
+    cache_path.mkdir(exist_ok=True)
     pass
 
     # yield
     yield
 
     # After the test
-    cache_path = Path(__file__).parent / "data" / ".eab_tools_cache"
     shutil.rmtree(cache_path)
 
 
@@ -31,3 +32,17 @@ class TestLoadDf:
         df = load_df(self.iris_csv)
 
         assert (df == iris).all(axis=None)
+
+    @pytest.mark.parametrize("file_type_specification", ["csv", "CSV", ".csv", ".CSV"])
+    def test_specify_file_type_csv(
+        self, file_type_specification: str, iris: pd.DataFrame
+    ) -> None:
+        # Make a copy of the csv with a weird extension
+        weird_file = Path(str(self.iris_csv) + ".foo")
+        shutil.copy(self.iris_csv, weird_file)
+
+        df = load_df(weird_file, file_type=file_type_specification)
+        assert (df == iris).all(axis=None)
+
+        # Clean up
+        weird_file.unlink()
