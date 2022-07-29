8aa82c7feba818e8fbdf945e37898dd7d4c0cade
Moshe Rubin <mosherubin137@gmail.com>
2022-07-28 19:58:17 -0500

    Add tests for xls file

--- EAB_tools/tests/io/test_io.py
+++ EAB_tools/tests/io/test_io.py
@@ -6,7 +6,7 @@ import pandas as pd
 import pytest
 
 from EAB_tools import load_df
-from EAB_tools._testing.types import PathLike
+import EAB_tools._testing as tm
 
 
 @pytest.fixture(autouse=True)
@@ -52,28 +52,20 @@ def all_mixups(file_extension: str) -> list[str]:
 @pytest.mark.parametrize("cache", [True, False], ids="cache={}".format)
 class TestLoadDf:
     data_dir = Path(__file__).parent / "data"
-    iris_csv = data_dir / "iris.csv"
-    iris_xlsx = data_dir / "iris.xlsx"
-    files = [
-        iris_csv,
-        iris_xlsx,
-    ]
+    files = list(data_dir.glob("iris*"))
 
     @pytest.mark.parametrize("file", files, ids=lambda pth: pth.name)
-    def test_doesnt_fail(self, cache: bool, file: PathLike) -> None:
+    def test_doesnt_fail(self, cache: bool, file: tm.PathLike) -> None:
         load_df(file, cache=cache)
 
     @pytest.mark.parametrize("file", files, ids=lambda pth: pth.name)
-    def test_load_iris(self, file: PathLike, iris: pd.DataFrame, cache: bool) -> None:
+    def test_load_iris(
+        self, file: tm.PathLike, iris: pd.DataFrame, cache: bool
+    ) -> None:
         df = load_df(file, cache=cache)
 
         assert (df == iris).all(axis=None)
 
-    # @pytest.mark.parametrize(
-    #     "file,file_type_specification",
-    #     [(file, all_mixups(file.suffix)) for file in files],
-    #     ids=lambda p: p.name if isinstance(p, Path) else p
-    # )
     @pytest.mark.parametrize(
         "file,file_type_specification",
         [
@@ -85,7 +77,7 @@ class TestLoadDf:
     )
     def test_specify_file_type(
         self,
-        file: PathLike,
+        file: tm.PathLike,
         file_type_specification: str,
         iris: pd.DataFrame,
         cache: bool,
@@ -113,8 +105,9 @@ class TestLoadDf:
             Path("test_path.bz2"),
         ],
     )
-    def test_pickle_name(self, file: PathLike, cache: bool, pkl_name: PathLike) -> None:
-        file = self.iris_csv
+    def test_pickle_name(
+        self, file: tm.PathLike, cache: bool, pkl_name: tm.PathLike
+    ) -> None:
         load_df(
             file,
             cache=True,  # Must be true to test pickling
@@ -125,4 +118,4 @@ class TestLoadDf:
         else:
             pkl_name = f"{pkl_name}.pkl.xz"
 
-        assert Path(self.iris_csv.parent / ".eab_tools_cache" / pkl_name).exists()
+        assert (Path(file).parent / ".eab_tools_cache" / pkl_name).exists()
