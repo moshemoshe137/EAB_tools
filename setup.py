e0f250db56bc8b5ce3ca2b0eba801d69933c7a6f
Moshe Rubin <mosherubin137@gmail.com>
2023-06-20 10:54:08 -0500

    Add `matplotlib` requirement

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ setuptools.setup(
     description="Tools for data exported from EAB",
     license="MIT",
     install_requires=[
-        "pandas",
-        "ipython",
         "dataframe_image",
+        "ipython",
+        "matplotlib",
+        "pandas",
     ],
 )
